268712fa20a3d74cebd6ecfd8fcd95fef7940d97
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-05-31 19:14:17 +0200

    Add files via upload

---
+++ (Clase 6) Añadir Datos a la Tabla.sql
@@ -0,0 +1,8 @@
+ -- Para insertar datos en una tabla, se tiene que hacer:
+
+ -- Recuerda no poner mas numeros o caracteres de los establecidos al declarar la tabla.
+ -- Para insertar una cadena de caracteres, tienes que ponerlo entre comillas simples('').
+ -- Al meter el precio en el "NUMBER(8,2)", no es obligatorio poner los decimales si el precio es entero.
+
+INSERT INTO MERCEDES (ID_MODELO, N_MODELO, PRECIO)
+VALUES (00003,'SLS',245640.65);
