d83235676ed05e988b4ef3090b29d8e69760a554
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-01 13:07:30 +0200

    Add files via upload

---
+++ (Clase 13) Mostrar datos de la Tabla III.sql
@@ -0,0 +1,5 @@
+-- Para sacar datos de una tabla, con condicion, haz:
+--(En este caso, solo muestra los Mercedes con ID superior a 1)
+SELECT ID_MODELO,N_MODELO,PRECIO
+FROM MERCEDES
+WHERE ID_MODELO>1;
