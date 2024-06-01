92ac25b1b45b6b13141b2758bc90f53d44f9df7f
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-01 12:26:42 +0200

    Update (Clase 9) Introducir fecha del sistema a columna.sql

--- (Clase 9) Introducir fecha del sistema a columna.sql
+++ (Clase 9) Introducir fecha del sistema a columna.sql
@@ -1,5 +1,6 @@
 -- Para insertar la fecha actual a una columna, escribe:
 --  Tommos como ejemplo una tabla EMPLEADOS, que le queremos 
 --  meter la fecha de hoy como el dia que mepezo a trabajar un trabajador.
+-- Recuerda que el dato debe ser tipo "DATE" para introducir la fecha del sistema.
 INSERT INTO EMPLEADOS
 VALUES (3456,'Jose',10234,SYSDATE);
