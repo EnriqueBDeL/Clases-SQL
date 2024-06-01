e82ef67c55d977b66f048cd3ed2d5dbe21237603
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-01 19:08:15 +0200

    Add files via upload

---
+++ (Clase 21) Mostrar datos de la Tabla XI.sql
@@ -0,0 +1,7 @@
+-- Muestra todos los datos de la tabla.
+-- SELECT * FROM VENTAS;  (Descomenta para usarlo)
+
+-- Actualiza un dato concreto en la tabla.
+UPDATE VENTAS
+SET PRECIO = 43.20
+WHERE ID_PRODUCTO = 101 AND ID = 8;
