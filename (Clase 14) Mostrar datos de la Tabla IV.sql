c4343544b2e0d6012fc2cef990841d8abc9441c4
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-01 13:12:08 +0200

    Add files via upload

---
+++ (Clase 14) Mostrar datos de la Tabla IV.sql
@@ -0,0 +1,5 @@
+-- Para sacar datos de una tabla, ordenado de forma ascendente o descendente, haz:
+--[Ascendente = ASC ||  Descendente = DESC]
+SELECT ID_MODELO,N_MODELO,PRECIO
+FROM MERCEDES
+ORDER BY ID_MODELO ASC
