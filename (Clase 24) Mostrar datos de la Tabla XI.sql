7f0d583806581b9283f8ac5c4dd3bd0b3edc16f4
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-03 11:56:19 +0200

    Update (Clase 24) Mostrar datos de la Tabla XI.sql

--- (Clase 24) Mostrar datos de la Tabla XI.sql
+++ (Clase 24) Mostrar datos de la Tabla XI.sql
@@ -1,3 +1,3 @@
--- Para imprimir en una tabla el valor de las tablas que quieras, haz:
+-- Para imprimir en una coumna el valor de las columnas que quieras, haz:
 SELECT ID_MODELO||N_MODELO AS "MODELOS"
 FROM MERCEDES;
