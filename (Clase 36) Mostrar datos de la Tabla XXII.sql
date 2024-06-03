153766f27a3be174b1b175dd9e78623645b73125
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-03 13:50:18 +0200

    Update (Clase 36) Mostrar datos de la Tabla XXII.sql

--- (Clase 36) Mostrar datos de la Tabla XXII.sql
+++ (Clase 36) Mostrar datos de la Tabla XXII.sql
@@ -1,3 +1,4 @@
 -- Pone a la izquierda cuantos caracteres quieras.
+-- (En el caso de que quieras hacer lo mismo pero hacia la derecha, usa "RPAD" en vez de "LPAD".)
 SELECT LPAD(ID_EMPLEADO, 5, '0') AS ID_EMPLEADO_COMPLETO
 FROM Empleados;
