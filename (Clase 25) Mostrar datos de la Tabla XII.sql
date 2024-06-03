1029fa9f4cf223088a763ee5892299422342b071
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-03 12:00:32 +0200

    Add files via upload

---
+++ (Clase 25) Mostrar datos de la Tabla XII.sql
@@ -0,0 +1,4 @@
+-- Para imprimir en una tabla el valor de las tablas que quieras, con un
+-- mensaje/caracter/espacio entre datos, haz:
+SELECT ID_MODELO||'_'||N_MODELO||'_'||PRECIO AS "MODELOS"
+FROM MERCEDES;
