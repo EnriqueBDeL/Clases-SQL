55336561c7460e8d71a15de14acec397b2da599d
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-03 17:24:43 +0200

    Add files via upload

---
+++ (Clase 39) Mostrar datos de la Tabla XXV.sql
@@ -0,0 +1,5 @@
+-- ROUND redondea a lo que tu quiereas un numero.
+-- TRUNC trunca el numero a lo que tu quieras.
+-- MOD devuelve el resto de la division.
+SELECT PRECIO, ROUND(PRECIO,2) AS "PRECIO REDONDEADO", TRUNC(PRECIO,2) AS "PRECIO TRUNCADO", MOD(PRECIO,30) AS "RESTO DE LA DIVISION"
+FROM MERCEDES;
