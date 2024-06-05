7a2cc7d0a04dcce87a1216113dd7e3bdd85aa746
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-06-05 10:45:30 +0200

    Add files via upload

---
+++ (Clase 72)Modos Tabla I.sql
@@ -0,0 +1,3 @@
+-- Si haces READ ONLY, no podrás modificar la tabala, solo leerla.
+
+ALTER TABLE MERCEDES READ ONLY;
---
+++ (Clase 73)Modos Tabla II.sql
@@ -0,0 +1,3 @@
+-- Si haces READ WRITE, podras leer y modificar la tabla.
+
+ALTER TABLE MERCEDES READ WRITE;
